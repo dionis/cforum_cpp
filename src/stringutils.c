d8df2dbc43128c7a0c9282f2e95894e17c3d4a90
ckruse <cjk@wwwtech.de>
2010-07-16 10:53:42 +0200

    added a small string abstraction

---
+++ src/stringutils.c
@@ -0,0 +1,133 @@
+/*!
+ * \file stringutils.c
+ * \author Christian Kruse, <cjk@wwwtech.de>
+ * \brief string utilities for the Classic Forum
+ *
+ * A string abstraction for the Classic Forum
+ */
+
+#include "config.h"
+#include "defines.h"
+
+#include <string.h>
+#include <stdlib.h>
+
+#include "stringutils.h"
+#include "memoryutils.h"
+
+void cf_str_init_growth(cf_string_t *str,unsigned growth) {
+  str->len      = 0;
+  str->reserved = 0;
+  str->growth   = growth;
+  str->content  = NULL;
+}
+
+void cf_str_cleanup(cf_string_t *str) {
+  str->len      = 0;
+  str->reserved = 0;
+
+  if(str->content) free(str->content);
+
+  str->content  = NULL;
+}
+
+size_t cf_str_char_append(cf_string_t *str,const char content) {
+  if(str->growth == 0) str->growth = CF_BUFSIZ;
+
+  if(str->len + 1 >= str->reserved) {
+    str->reserved += str->growth;
+    str->content   = cf_alloc(str->content,(size_t)str->reserved,1,CF_ALLOC_REALLOC);
+  }
+
+  str->content[str->len] = content;
+  str->len              += 1;
+  str->content[str->len] = '\0';
+
+  return 1;
+}
+
+size_t cf_str_chars_append(cf_string_t *str,const char *content,size_t length) {
+  size_t len;
+
+  if(str->growth == 0) str->growth = CF_BUFSIZ;
+  len = str->growth;
+
+  if(str->len + length >= str->reserved) {
+    if(length >= len) len += length;
+
+    str->reserved += len;
+    str->content   = cf_alloc(str->content,(size_t)str->reserved,1,CF_ALLOC_REALLOC);
+  }
+
+  memcpy(&str->content[str->len],content,length);
+  str->len += length;
+  str->content[str->len] = '\0';
+
+  return length;
+}
+
+int cf_str_equal_string(const cf_string_t *str1,const cf_string_t *str2) {
+  register char *ptr1 = str1->content,*ptr2 = str2->content;
+  register size_t i;
+
+  if(str1->len != str2->len) return 1;
+
+  for(i = 0; i < str1->len; ++i,++ptr1,++ptr2) {
+    if(*ptr1 != *ptr2) return 1;
+  }
+
+  return 0;
+}
+
+int cf_str_equal_chars(const cf_string_t *str1,const char *str2, size_t len) {
+  register size_t i = 0;
+  register char *ptr1 = str1->content,*ptr2 = (char *)str2;
+
+  if(str1->len != len) return 1;
+
+  for(i = 0; i < len; ++i,++ptr1,++ptr2) {
+    if(*ptr1 != *ptr2) return 1;
+  }
+
+  return 0;
+}
+
+size_t cf_str_str_append(cf_string_t *str,cf_string_t *content) {
+  return cf_str_chars_append(str,content->content,content->len);
+}
+
+size_t cf_str_cstr_append(cf_string_t *str,const char *content) {
+  return cf_str_chars_append(str,content,strlen(content));
+}
+
+int cf_str_cstr_set(cf_string_t *str,const char *content) {
+  return cf_str_char_set(str,content,strlen(content));
+}
+
+size_t cf_str_char_set(cf_string_t *str,const char *content,size_t length) {
+  size_t len;
+
+  if(str->growth == 0) str->growth = CF_BUFSIZ;
+  len = str->growth;
+
+  if(str->len + length >= str->reserved) {
+    if(length >= len) len += length;
+
+    str->reserved  = len;
+    str->content   = cf_alloc(str->content,len,1,CF_ALLOC_REALLOC);
+  }
+
+  memcpy(str->content,content,length);
+  str->len = length;
+  str->content[length] = '\0';
+
+  return length;
+}
+
+size_t cf_str_str_set(cf_string_t *str,cf_string_t *set) {
+  return cf_str_char_set(str,set->content,set->len);
+}
+
+
+
+/* eof */
---
+++ src/stringutils.h
@@ -0,0 +1,122 @@
+/*!
+ * \file stringutils.c
+ * \author Christian Kruse, <cjk@wwwtech.de>
+ * \brief string utilities for the Classic Forum
+ *
+ * A string abstraction for the Classic Forum
+ */
+
+#ifndef CF_STRING_UTILS_H
+#define CF_STRING_UTILS_H
+
+/*!
+ * This is the heart of the string abstraction. It contains the string itself, the length
+ * of the string and the size of the reserved memory for the string.
+ */
+typedef struct s_string {
+  unsigned long len; /*!< length of the memory used */
+  unsigned long reserved; /*!< length of the memory area itself */
+  unsigned growth;
+  char *content; /*!< The memory area itself */
+} cf_string_t;
+
+#define STRING_INITIALIZER { 0, 0, CF_BUFSIZ, NULL }
+
+/*!
+ * This function initializes a string structure with specified growth factor.
+ * \param \c str A reference to a string structure
+ * \param \c growth The growth factor
+ */
+void cf_str_init_growth(cf_string_t *str,unsigned growth);
+
+#define cf_str_init(str) cf_str_init_growth((str),CF_BUFSIZ)
+
+/*!
+ * This function frees the reserved memory in a string structure and sets everything to NULL
+ * \param \c str A reference to the string structure
+ */
+void cf_str_cleanup(cf_string_t *str);
+
+/*!
+ * This function appends a character to the string
+ * \param \c str A reference to the string structure
+ * \param \c content The character to append
+ * \return Number of characters appended on success or 0 on failure
+ */
+size_t cf_str_char_append(cf_string_t *str,const char content);
+
+/*!
+ * This function appends a char array to the string in the string structure.
+ * \param \c str A reference to the string structure
+ * \param \c content The char array to append
+ * \param \c length The length of the char array
+ * \return The number of characters appended on success or 0 on failure
+ */
+size_t cf_str_chars_append(cf_string_t *str,const char *content,size_t length);
+
+/*!
+ * This function appends a string structure to a string structure. It's just a wrapper for
+ * the cf_str_chars_append() function.
+ * \param \c str A reference to the string structure to append to
+ * \param \c content A reference to the string structure to append
+ */
+size_t cf_str_str_append(cf_string_t *str,cf_string_t *content);
+
+/*!
+ * This function appends a C-like null terminated character array to a string structure.
+ * It's just a wrapper for the cf_str_chars_append() function.
+ * \param \c str A reference to the string structure to append to
+ * \param \c content The char array to append
+ * \return The number of characters appended on success or 0 on failure
+ */
+size_t cf_str_cstr_append(cf_string_t *str,const char *content);
+
+/*!
+ * This function sets the value of an string structure to a given char array. The old string contained
+ * in the structure will be lost. It gets the length of the char array by strlen().
+ * \param \c str A reference to the string to append to
+ * \param \c content The string to set
+ * \return The number of characters set on success or 0 on failure
+ */
+int cf_str_cstr_set(cf_string_t *str,const char *content);
+
+/*!
+ * This function sets the value of an string structure to a given char array. The old string contained
+ * in the structure will be lost.
+ * \param \c str A reference to the string to append to
+ * \param \c content The string to set
+ * \param \c length The length of the string to set
+ * \return The number of characters set on success or 0 on failure
+ */
+size_t cf_str_char_set(cf_string_t *str,const char *content,size_t length);
+
+/*!
+ * This function sets the value of a string structure to the value of another string structure. The old
+ * string contained in the target structure will be lost.
+ * \param \c str A reference to the structure to set to
+ * \param \c content A reference to the structure to set
+ * \return The number of characters set on success or 0 on failure
+ */
+size_t cf_str_str_set(cf_string_t *str,cf_string_t *content);
+
+/*!
+ * This function tests if two strings (cf_string_t) are equal
+ * \param \c str1 string 1
+ * \param \c str2 string 2
+ * \return TRUE if both equal, FALSE otherwise
+ */
+int cf_str_equal_string(const cf_string_t *str1,const cf_string_t *str2);
+
+/*!
+ * This function tests if two strings (cf_string_t, char *) are equal
+ * \param \c str1 string 1
+ * \param \c str2 string 2
+ * \param \c len Length of string str2
+ * \return TRUE if both equal, FALSE otherwise
+ */
+int cf_str_equal_chars(const cf_string_t *str1,const char *str2, size_t len);
+
+
+#endif
+
+/* eof */
