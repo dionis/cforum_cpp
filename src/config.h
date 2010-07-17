825c79821e615b55f60e28290232cadb846769c5
ckruse <cjk@wwwtech.de>
2010-07-17 10:17:25 +0200

    added macros to compare float and double values

--- src/config.h
+++ src/config.h
@@ -15,6 +15,13 @@
 
 #define CF_PRERESERVE 5 /*!< default prereservation count (e.g. for arrays) */
 
+#define CF_DBL_EPSILON    2.2204460492503131e-016 /* smallest value for 1.0+DBL_EPSILON != 1.0 */
+#define CF_FLT_EPSILON    1.192092896e-07F /* smallest value for 1.0+FLT_EPSILON != 1.0 */
+
+#define CF_FLOAT_EQ(x,v) ((((v) - CF_FLT_EPSILON) < (x)) && ((x) < ((v) + CF_FLT_EPSILON)))
+#define CF_DOUBL_EQ(x,v) ((((v) - CF_DBL_EPSILON) < (x)) && ((x) < ((v) + CF_DBL_EPSILON)))
+
+
 #endif
 
 /* eof */
