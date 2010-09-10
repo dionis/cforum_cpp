3198f3bd59627248b067054df2c1619e88e0b46a
ckruse <cjk@wwwtech.de>
2010-09-10 23:06:08 +0200

    added kqueue(2) implementation of main loop

--- src/cf_server_kqueue.c
+++ src/cf_server_kqueue.c
@@ -7,7 +7,72 @@
  * the BSD's kqueue(2) API.
  */
 
+#include <sys/event.h>
+#include <sys/time.h>
+
+#include "serverlib.h"
+
+static int kq = -1,nchanges = 0;
+static struct kevent change;
+
 int cf_main_loop(cf_server_context_t *context) {
+  struct kevent *event;
+  cf_list_element_t *elem;
+  cf_listener_t *srv;
+  struct timespec timeout;
+  int numev,i,connfd;
+  socklen_t size;
+
+  if(kq == -1) {
+    kq = kqueue();
+    if(kq == -1) {
+      CF_ERROR(context,"Error in kqueue(): %s",strerror(errno));
+      return -1;
+    }
+
+    memset(&change,0,sizeof(change));
+    for(elem = context->listeners.elements;elem;elem = elem->next,++nchanges) {
+      srv = (cf_listener_t *)elem->data;
+      EV_SET(&change,srv->sock,EVFILT_READ,EV_ADD|EV_ENABLE,0, 0, 0);
+    }
+  }
+
+  event = cf_alloc(NULL,nchanges,sizeof(*event),CF_ALLOC_CALLOC);
+
+  timeout.tv_sec = 10;
+  timeout.tv_nsec = 0;
+
+  numev = kevent(kq,&change,nchanges,event,nchanges,&timeout);
+  CF_LOG(context,CF_LOG_DBG(3),"Returned from kqueue: %d",numev);
+
+  if(numev == -1) {
+    CF_ERROR(context,"Error in kevent(): %s",strerror(errno));
+    free(event);
+    return -1;
+  }
+  else if(numev > 0) {
+    for(i=0;i<numev;++i) {
+      for(elem = context->listeners.elements;elem;elem = elem->next) {
+        srv = (cf_listener_t *)elem->data;
+
+        if((uintptr_t)srv->sock == event[i].ident) {
+          size = srv->size;
+          connfd = accept(srv->sock,srv->addr,&size);
+
+          /* accept-error? */
+          if(connfd <= 0) {
+            CF_ERROR(context,"accept: %s\n",strerror(errno));
+            return 0;
+          }
+
+          cf_srv_append_client(context,connfd,srv);
+        }
+      }
+    }
+  }
+
+  free(event);
+
   return 0;
 }
 
