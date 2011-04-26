f20fe9d39fa7ffd1dd1958ff070d3e80c5750eb1
Christian Kruse <cjk@wwwtech.de>
2011-04-26 22:33:09 +0200

    added hash map abstraction

---
+++ src/hash_map.h
@@ -0,0 +1,54 @@
+/**
+ * \author Christian Kruse <cjk@wwwtech.de>
+ * \brief abstracts the hash map debacle
+ * \package JSON
+ *
+ * This simply abstracts the hash_map debacle of std::hash_map
+ */
+
+/*
+ * Copyright (C) 2011 by Christian Kruse <cjk@wwwtech.de>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef HASH_MAP_H
+#define HASH_MAP_H
+
+#ifdef __GNUC__
+#include <ext/hash_map>
+#else
+#include <hash_map>
+#endif
+
+namespace std {
+  using namespace __gnu_cxx;
+}
+
+namespace CForum {
+  struct hash_unicodestring {
+    std::size_t operator()(const UnicodeString &ustr) const {
+      return static_cast<std::size_t>(ustr.hashCode());
+    }
+  };
+}
+
+# endif
+
+/* eof */
