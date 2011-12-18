8eace51d4b4cc416a76097c8e13d948d6e958570
Christian Kruse <cjk@wwwtech.de>
2011-12-18 22:23:25 +0100

    added script to generate couchdb views

---
+++ scripts/gen_views.rb
@@ -0,0 +1,23 @@
+#!/usr/local/bin/ruby -wKu
+# -*- coding: utf-8 -*-
+
+require 'couchrest'
+require 'json'
+
+views_file = "./etc/views.js"
+db_uri = "http://127.0.0.1:5984/cforum"
+
+views_file = ARGV[0] if ARGV.length > 0
+db_uri = ARGV[1] if ARGV.length > 1
+
+db = CouchRest.database!(db_uri)
+views = JSON.parse(File.read(views_file))
+
+begin
+  db.save_doc(views)
+rescue
+  puts "Error creating views: " + $!.to_s
+end
+
+
+# eof
