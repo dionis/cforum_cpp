c44a2569e4bf254b613a7cc814045524ffe316bc
Christian Kruse <cjk@wwwtech.de>
2011-12-18 22:23:38 +0100

    added some basic views (by_tid and by_month)

---
+++ etc/views.js
@@ -0,0 +1,12 @@
+{
+  "_id": "_design/cforum",
+  "language": "javascript",
+  "views": {
+    "by_tid": {
+      "map": "function(doc) { if(doc.tid) emit(doc.tid); }"
+    },
+    "by_month": {
+      "map": "function(doc) { emit(doc.messages[0].date.substr(0,7)); }"
+    }
+  }
+}
