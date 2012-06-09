d686ec24869f4d7fe99f8f8f1c63250d898a6a66
Christian Kruse <cjk@wwwtech.de>
2012-06-09 13:18:07 +0200

    added a model for threads

---
+++ src/models/thread.cc
@@ -0,0 +1,80 @@
+/**
+ * \author Christian Kruse <cjk@wwwtech.de>
+ * \brief CForum thread model implementation
+ *
+ * CForum thread model implementation
+ */
+
+/*
+ * Copyright (C) 2011 by Christian Kruse <cjk@wwwtech.de>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include "models/thread.hh"
+
+namespace CForum {
+  namespace Models {
+    Thread::Thread() : Model(), id(), tid(), messages(), archived(false) { }
+    Thread::Thread(const Thread &t) : Model(), id(t.id), tid(t.tid), messages(t.messages), archived(t.archived) { } // TODO: create a COPY of t.messages
+
+    Thread &Thread::operator=(const Thread &t) {
+      if(this != &t) {
+        tid      = t.tid;
+        id       = t.id;
+        messages = t.messages;  // TODO: create a COPY of messages
+        archived = t.archived;
+      }
+
+      return *this;
+    }
+
+    Thread::~Thread() { }
+
+    boost::shared_ptr<Thread> Thread::fromJSON(const JSON::Object &) { // TODO: implement
+      return boost::shared_ptr<Thread>();
+    }
+
+    boost::shared_ptr<Thread> Thread::fromBSON(const mongo::BSONObj &o) {
+      boost::shared_ptr<Thread> t = boost::make_shared<Thread>();
+
+      t->tid      = o.getField("tid").String();
+      t->id       = o.getField("_id").String();
+      t->archived = o.getField("archived").Bool();
+
+      const std::vector<mongo::BSONElement> msgs = o.getField("messages").Array();
+      std::vector<mongo::BSONElement>::const_iterator it, end = msgs.end();
+      for(it = msgs.begin(); it != end; ++it) {
+        t->messages.push_back(Message::fromBSON(it->Obj()));
+      }
+
+      return t;
+    }
+
+    boost::shared_ptr<JSON::Element> Thread::toJSON() {
+      return boost::shared_ptr<JSON::Object>();
+    }
+
+    boost::shared_ptr<mongo::BSONObj> Thread::toBSON() {
+      return boost::shared_ptr<mongo::BSONObj>();
+    }
+  }
+}
+
+/* eof */
---
+++ src/models/thread.hh
@@ -0,0 +1,68 @@
+/**
+ * \author Christian Kruse <cjk@wwwtech.de>
+ * \brief CForum thread model
+ *
+ * CForum thread model
+ */
+
+/*
+ * Copyright (C) 2011 by Christian Kruse <cjk@wwwtech.de>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef THREAD_H
+#define THREAD_H
+
+#include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
+
+#include "json/json_parser.hh"
+#include "framework/model.hh"
+#include "framework/mongodb.hh"
+
+#include "models/message.hh"
+
+namespace CForum {
+  namespace Models {
+    class Thread : public Model {
+    public:
+      Thread();
+      Thread(const Thread &);
+
+      virtual Thread &operator=(const Thread &);
+
+      virtual ~Thread();
+
+      static boost::shared_ptr<Thread> fromJSON(const JSON::Object &);
+      static boost::shared_ptr<Thread> fromBSON(const mongo::BSONObj &);
+
+      virtual boost::shared_ptr<JSON::Element> toJSON();
+      virtual boost::shared_ptr<mongo::BSONObj> toBSON();
+
+      std::string id, tid;
+      std::vector<boost::shared_ptr<Message> > messages;
+      bool archived;
+    };
+  }
+}
+
+#endif
+
+/* eof */
