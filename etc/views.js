c9fe7127454c52b1a6969e21db7d2eb895da7042
Christian Kruse <cjk@wwwtech.de>
2012-06-14 23:35:54 +0200

    added a JS version of the absURL() method

--- etc/views.js
+++ etc/views.js
@@ -6,5 +6,20 @@ function out(val) {
   _e(val);
 }
 
+function absURL(thread, message, method, query) {
+  var base = configparser.getByPath("system/urls/base", false);
+  var url = base + thread.id;
+  if(message) {
+    url += "/" + message.id;
+  }
+  if(method) {
+    url += ";" + method;
+  }
+  if(query) {
+    url += "?" + url;
+  }
+
+  return url;
+}
 
 
